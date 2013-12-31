6c92cc61741a39eccb80f320230791a23323b6c8
kxxoling <kxxoling@gmail.com>
2013-12-31 13:27:58 +0800

    Add a README file; add recovery mode; bug fixes;

---
+++ README.md
@@ -0,0 +1,19 @@
+How to use
+----------
+
+1. Open your Mac terminal
+2. cd to this folder
+3. Run <code>python trash.py</code>
+4. Enter your password to permit
+
+
+Exceptions
+----------
+
+1. Trash icons doesn't change: Try to empty your Trash or add something to Trash.
+
+
+Thanks
+------
+
+Thanks to MacPro Trash Icons provider [神楽坂](https://plus.google.com/+%E7%A5%9E%E6%A5%BD%E5%9D%82%E9%9B%AF%E9%BA%97/posts)
--- trash.py
+++ trash.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 #author: kxxoling
 
-import os
+import os, sys
 
 TRASH_PATH = '/System/Library/CoreServices/Dock.app/Contents/Resources'
 FILES = ['trashempty.png',
@@ -14,10 +14,27 @@ FILES = ['trashempty.png',
 
 
 def replace_file(somefile, source=os.path.join(os.getcwd(), 'macpro/')):
-    sh = 'sudo cp %s %s' % (os.path.join(source, f), os.path.join(TRASH_PATH, somefile))
+    sh = 'sudo cp %s %s' % (os.path.join(source, somefile), os.path.join(TRASH_PATH, somefile))
     os.popen(sh)
 
-if __name__ == '__main__':
+
+def macpro_trash():
     for f in FILES:
         replace_file(f)
     os.popen('killall Dock')
+    print 'Replce Maverick Trash Icons with MacPro icons successfully!'
+
+
+def recovery():
+    for f in FILES:
+        replace_file(f, source=os.path.join(os.getcwd(), 'maverick/'))
+    os.popen('killall Dock')
+    print 'Recover Maverick Trash Icons successfully!'
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        macpro_trash()
+    elif sys.argv[1] == '-r':
+        recovery()
+    print "If this don't work, try to empty your Trash or add something to Trash."
